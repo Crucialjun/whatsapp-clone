b18e9dc7304adb9ffbe25b80cb949ca97f9772a5
Crucialjun <nich.otieno@gmail.com>
2022-09-08 23:25:41 +0300

    add giphy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,9 +16,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  //await FirebaseAppCheck.instance.activate(
-  //webRecaptchaSiteKey: 'recaptcha-v3-site-key',
-  //);
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
